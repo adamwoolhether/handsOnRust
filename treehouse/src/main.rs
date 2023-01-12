f8d9171fd7da81a647d3009d587bed543b621530
adamwoolhether <adamwoolhether@gmail.com>
2023-01-12 23:47:16 +0800

    re-added initial code to workspace

---
+++ treehouse/src/main.rs
@@ -0,0 +1,98 @@
+use std::io::stdin;
+/*
+Debug placeholders for can be used for any type that supports the Debug trait.
+
+{:?}
+{:?}    raw
+{:#?}   pretty-print
+*/
+
+#[derive(Debug)]
+enum VisitorAction {
+    Accept,
+    AcceptWithNote { note: String },
+    Refuse,
+    Probation,
+}
+
+#[derive(Debug)]
+struct Visitor {
+    name: String,
+    action: VisitorAction,
+    age: i8,
+}
+impl Visitor {
+    fn new(name: &str, action: VisitorAction, age: i8) -> Self {
+        // accept str, but store as String.
+        Self {
+            name: name.to_lowercase(),
+            action,
+            age,
+        }
+    }
+
+    fn greet_visitor(&self) {
+        match &self.action {
+            VisitorAction::Accept => println!("Welcome to the treehouse, {}", self.name),
+            VisitorAction::AcceptWithNote { note } => {
+                println!("Welcome to the treehouse, {}", self.name);
+                println!("{}", note);
+                if self.age < 21 {
+                    println!("Do not serve alcohol to {}", self.name);
+                }
+            }
+            VisitorAction::Probation => println!("{} is now a probationary member", self.name),
+            VisitorAction::Refuse => println!("Do not allow {} in!", self.name),
+        }
+    }
+}
+
+fn name() -> String {
+    let mut your_name = String::new();
+    stdin()
+        .read_line(&mut your_name)
+        .expect("Failed to read line");
+    your_name
+        .trim() // remove non-print chars like \n, \r, etc.
+        .to_lowercase()
+}
+
+fn main() {
+    let mut guest_list = vec![
+        Visitor::new("bert", VisitorAction::Accept, 45),
+        Visitor::new(
+            "steve",
+            VisitorAction::AcceptWithNote {
+                note: String::from("Lactose-free milk is in the fridge."),
+            },
+            15,
+        ),
+        Visitor::new("fred", VisitorAction::Refuse, 30),
+    ];
+
+    loop {
+        println!("Hello, what's your name? (Leave empty and press ENTER to quit");
+
+        let name = name();
+
+        let known_guest = guest_list
+            .iter() // using an iterator instead of looping and returning bool!
+            .find(|visitor| visitor.name == name); // find() will run a `closure`, returning an `Option`
+
+        // run a match statement on the `Option` returned by find()
+        match known_guest {
+            Some(visitor) => visitor.greet_visitor(),
+            None => {
+                if name.is_empty() {
+                    break;
+                } else {
+                    println!("{} is not on the guest list.", name);
+                    guest_list.push(Visitor::new(&name, VisitorAction::Probation, 0));
+                }
+            }
+        }
+    }
+
+    println!("The final list of visitors:");
+    println!("{:?}", guest_list)
+}
