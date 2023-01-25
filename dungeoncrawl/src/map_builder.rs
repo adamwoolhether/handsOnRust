7c7bbf50ffdffb837dca501504e22123c8a5fc3e
adamwoolhether <adamwoolhether@gmail.com>
2023-01-25 21:14:18 +0800

    build basic dungeon and corridors/rooms

---
+++ dungeoncrawl/src/map_builder.rs
@@ -0,0 +1,97 @@
+use crate::prelude::*;
+const NUM_ROOMS: usize = 20;
+
+// MapBuilder contains its own map, works on it's own copy, and
+// passes the results to the game.
+pub struct MapBuilder {
+    pub map: Map,
+    pub rooms: Vec<Rect>, // Rect is a structure from `bracket-lib` that helps calculations involving rectangles.
+    pub player_start: Point, // Where the player enters the map.
+}
+
+impl MapBuilder {
+    fn fill(&mut self, tile: TileType) {
+        self.map.tiles.iter_mut().for_each(|t| *t = tile); // * de-referenes t. We want to write to the references var, not the reference itself.
+    }
+
+    fn build_random_rooms(&mut self, rng: &mut RandomNumberGenerator) {
+        // Keep generating rooms until there are NUM_ROOMS on the map.
+        while self.rooms.len() < NUM_ROOMS {
+            // Generate a randomly positioned room.
+            let room = Rect::with_size(
+                rng.range(1, SCREEN_WIDTH - 10),
+                rng.range(1, SCREEN_HEIGHT - 10),
+                rng.range(2, 10),
+                rng.range(2, 10),
+            );
+            let mut overlap = false;
+            for r in self.rooms.iter() {
+                if r.intersect(&room) {
+                    overlap = true;
+                }
+            }
+            if !overlap {
+                room.for_each(|p| {
+                    // Rect::for_each runs the provided closure on every x/y coordinate inside the represented rectangle.
+                    if p.x > 0 && p.x < SCREEN_WIDTH && p.y > 0 && p.y < SCREEN_HEIGHT {
+                        let idx = map_idx(p.x, p.y);
+                        self.map.tiles[idx] = TileType::Floor;
+                    }
+                });
+                self.rooms.push(room)
+            }
+        }
+    }
+
+    fn apply_vertical_tunnel(&mut self, y1: i32, y2: i32, x: i32) {
+        use std::cmp::{max, min};
+        for y in min(y1, y2)..=max(y1, y2) {
+            if let Some(idx) = self.map.try_idx(Point::new(x, y)) {
+                self.map.tiles[idx as usize] = TileType::Floor;
+            }
+        }
+    }
+
+    fn apply_horizontal_tunnel(&mut self, x1: i32, x2: i32, y: i32) {
+        use std::cmp::{max, min};
+        for x in min(x1, x2)..=max(x1, x2) {
+            if let Some(idx) = self.map.try_idx(Point::new(x, y)) {
+                self.map.tiles[idx as usize] = TileType::Floor;
+            }
+        }
+    }
+
+    fn build_corridors(&mut self, rng: &mut RandomNumberGenerator) {
+        let mut rooms = self.rooms.clone();
+        // vectors' `sort_by` method requires a closure - an inline function that calls cmp() on two elements of the
+        // vector's contents.
+        rooms.sort_by(|a, b| a.center().x.cmp(&b.center().x));
+
+        // enumerate counts items in the iterator and includes them as the first entry in a tuple.
+        for (i, room) in rooms.iter().enumerate().skip(1) {
+            let prev = rooms[i - 1].center();
+            let new = room.center();
+
+            if rng.range(0, 2) == 1 {
+                self.apply_horizontal_tunnel(prev.x, new.x, prev.y);
+                self.apply_vertical_tunnel(prev.y, new.y, new.x);
+            } else {
+                self.apply_horizontal_tunnel(prev.y, new.y, prev.x);
+                self.apply_vertical_tunnel(prev.x, new.x, new.y);
+            }
+        }
+    }
+
+    pub fn new(rng: &mut RandomNumberGenerator) -> Self {
+        let mut mb = MapBuilder {
+            map: Map::new(),
+            rooms: Vec::new(),
+            player_start: Point::zero(),
+        };
+        mb.fill(TileType::Wall);
+        mb.build_random_rooms(rng);
+        mb.build_corridors(rng);
+        mb.player_start = mb.rooms[0].center();
+        mb
+    }
+}
